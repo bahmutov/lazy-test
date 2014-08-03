38f824e2493eceab994a00d9c413e3e3c7d08b1b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-08-03 17:04:05 -0400

    safer logger

--- lazy-test.js
+++ lazy-test.js
@@ -4,10 +4,13 @@
   var testScheduled = false;
 
   var started = +(new Date());
-  console.ts = function () {
-    var args = Array.prototype.slice.call(arguments, 0);
-    args.unshift(String(new Date() - started));
-    console.log.apply(console, args);
+  function log() {
+    if (typeof console === 'object' &&
+      typeof console.log === 'function') {
+      var args = Array.prototype.slice.call(arguments, 0);
+      args.unshift(String(new Date() - started));
+      console.log.apply(console, args);
+    }
   }
 
   // defer given function - just add it to the event queue
@@ -23,7 +26,7 @@
       delay = 0;
     }
     if (root.lazyTest.options.debug) {
-      console.ts('scheduling test', testScheduled, 'delay', delay);
+      log('scheduling test', testScheduled, 'delay', delay);
     }
     if (testScheduled) {
       return;
@@ -41,19 +44,19 @@
   function it(name, fn) {
     var testCallback = function () {
       if (root.lazyTest.options.verbose) {
-        console.ts('starting test', this.name);
+        log('starting test', this.name);
       }
 
       testScheduled = false;
       try {
         fn();
         if (root.lazyTest.options.verbose) {
-          console.ts('test "' + this.name + '" passed');
+          log('test "' + this.name + '" passed');
         }
       } catch (err) {
         if (root.lazyTest.options.verbose) {
-          console.ts('test "' + this.name + '" failed');
-          console.ts(err.message);
+          log('test "' + this.name + '" failed');
+          log(err.message);
         }
       }
       root.lazyTest.start();
